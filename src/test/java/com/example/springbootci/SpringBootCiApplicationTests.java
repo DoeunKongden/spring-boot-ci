78d14a99d99a102d987a2d3d16b9098a9d91fa72
DoeunKongden <doeunkongden1@gmail.com>
2025-12-02 08:21:21 +0700

    testing testing

---
+++ src/test/java/com/example/springbootci/SpringBootCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
